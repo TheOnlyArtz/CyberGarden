9952c048acfc6112a7f37f34665665c00a5cfb35
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 19:41:22 +0200

    A new somewhat automated shop command

--- src/cybergarden/commands/shop.cr
+++ src/cybergarden/commands/shop.cr
@@ -1,4 +1,19 @@
+# pp Cybergarden::Items::CpuTypes[0].price
+DESCRIPTION = Cybergarden::Items::CpuTypes.values.map { |x|
+    curr = Cybergarden::Items::CpuTypes.from_value(x.to_i).to_cpu
+    curr.description
+}.join("\n")
+
 def Cybergarden::Commands.shop(cybergarden : Cybergarden::Client,
                                         message : Discord::Message,
                                         args : Array(String)) : Nil
+
+    embed = Discord::Embed.new(
+        title: "The great hardware shop!",
+        timestamp: Time.utc,
+        description: DESCRIPTION,
+        footer: Discord::EmbedFooter.new("To buy a new CPU go ahead and type #{Cybergarden::PREFIX}buy CPU <number> (e.g: buy CPU 0)")
+    )
+
+    cybergarden.client.create_message(message.channel_id, content: "", embed: embed)
 end
