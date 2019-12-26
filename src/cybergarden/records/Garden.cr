414f3514c1505d85b699845bbc22c6226791f5e6
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 16:59:08 +0200

    Added a Garden type

---
+++ src/cybergarden/records/Garden.cr
@@ -0,0 +1,26 @@
+require "./Server"
+require "./ServerTypes"
+
+struct Cybergarden::Garden
+    getter servers : Array(Cybergarden::Items::Server)
+    getter owner_id : String # UInt64 for Discord.cr
+    getter last_money_gain : Int32
+    getter money : Int32
+
+    def initialize(payload : Hash(String, RethinkDB::QueryResult))
+        @owner_id = payload["id"].to_s
+        @money = payload["money"].as_i
+        @last_money_gain = payload["last_money_gain"].as_i
+        @servers = payload["servers"].as_a.map { |server|
+            Cybergarden::Items::Server
+            .from_h(server.as_h)
+            .as(Cybergarden::Items::Server)
+        }
+    end
+
+    def get_mps()
+        mps = 0
+        servers.each {|server| mps += server.get_mps}
+        mps
+    end
+end
