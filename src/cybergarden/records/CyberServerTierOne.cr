b549519aca61d0cfc0932279e1ef3f4ed5d086bc
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 16:59:28 +0200

    First versions of CPU and Server

---
+++ src/cybergarden/records/CyberServerTierOne.cr
@@ -0,0 +1,18 @@
+require "./Server"
+require "./CpuTypes"
+module Cybergarden::Items
+    struct CyberServerTierOne < Cybergarden::Items::Server
+        def initialize(processors : Array(RethinkDB::QueryResult))
+            @capacity = 20
+            @price = 10000
+            @maintability = 1 # fairly low
+            @type = 0
+
+            @processors = processors.map { |cpu|
+                Cybergarden::Items::CpuTypes[cpu["type"].as_i]
+                .new
+                .as(Cybergarden::Items::Cpu)
+            }
+        end
+    end
+end
