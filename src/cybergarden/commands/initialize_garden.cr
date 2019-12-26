590241e7cf62e9fe46a676b33d2f9c9002724e02
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 17:06:54 +0200

    Added a new command to initialize a garden

---
+++ src/cybergarden/commands/initialize_garden.cr
@@ -0,0 +1,27 @@
+def Cybergarden::Commands.initialize_garden(cybergarden : Cybergarden::Client,
+                                            message : Discord::Message,
+                                             args : Array(String)) : Nil
+                                             
+    garden = get_garden(message.author.id, cybergarden)
+    if garden != nil
+    cybergarden.client.create_message(message.channel_id, "Hey TheOnlyArtz, You already own a garden, type ;garden to see it.")
+    return
+    end
+    
+    payload = {
+    id: message.author.id.to_s,
+    money: 200,
+    last_money_gain: Time.utc.to_unix,
+    servers: [] of Nil
+    }
+
+    r.db("cybergarden")
+    .table("gardens")
+    .insert(payload)
+    .run(cybergarden.rethink.connection)
+
+    content = "Congrats #{message.author.username}! I've created a fresh new garden for you!"
+    cybergarden.client.create_message(message.channel_id, content)
+
+    nil
+end
