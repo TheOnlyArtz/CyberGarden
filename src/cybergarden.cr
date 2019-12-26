dc8258dba3f8e3ad5a5ebdce2ffa15c846c8153e
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 17:17:51 +0200

    Code cleanup

--- src/cybergarden.cr
+++ src/cybergarden.cr
@@ -1,6 +1,39 @@
+
 # TODO: Write documentation for `Cybergarden`
+require "crystal-rethinkdb"
+require "json"
+
+require "./cybergarden/index.cr"
+require "./cybergarden/records/CyberServerTierOne"
+require "./cybergarden/records/Garden"
+require "./cybergarden/utils/*"
+require "./cybergarden/commands/*"
+
+
+include RethinkDB::Shortcuts
+include Cybergarden::Utilities
+
 module Cybergarden
   VERSION = "0.1.0"
-
-  # TODO: Put your code here
+  PREFIX = ";"
 end
+
+cybergarden = Cybergarden::Client.new()
+
+cybergarden.client.on_message_create  do |msg|
+  if !msg.content.starts_with? Cybergarden::PREFIX
+    next
+  end
+
+  tokens = msg.content.split(';')[1].split(' ')
+  command = tokens[0]
+  tokens.delete_at(0)
+
+  if command == "init"
+    Cybergarden::Commands.initialize_garden(cybergarden, msg, tokens)
+  elsif command == "stats"
+    Cybergarden::Commands.garden_stats(cybergarden, msg, tokens)
+  end
+end
+
+cybergarden.client.run
