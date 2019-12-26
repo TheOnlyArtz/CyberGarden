81b8c51bcefde049a74a7e475fc506f7214d9a4c
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 21:32:55 +0200

    Static getters

--- src/cybergarden/records/CyberServerTierOne.cr
+++ src/cybergarden/records/CyberServerTierOne.cr
@@ -1,17 +1,27 @@
 require "./Server"
 require "./CpuTypes"
+
 module Cybergarden::Items
     struct CyberServerTierOne < Cybergarden::Items::Server
+        class_getter price : Int32 = 10000
+        class_getter maintability : Int32 = 0
+        class_getter capacity : Int32 = 20
+        class_getter type : Int32 = 0
+
         def initialize(processors : Array(RethinkDB::QueryResult), @name : String)
-            @capacity = 20
-            @price = 10000
-            @maintability = 1 # fairly low
-            @type = 0
+            @capacity = @@capacity
+            @price = @@price
+            @maintability = @@maintability # fairly low
+            @type = @@type
 
             @processors = processors.map { |cpu|
                 Cybergarden::Items::CpuTypes
                 .from_value(cpu["type"].as_i).to_cpu
             }
+        end
+
+        def self.description
+         "#{@@type}) **Name:** #{self.name.split("Cybergarden::Items::")[1]} | **Price:** #{@@price}$ | **Capacity:** #{@@capacity}"
         end
     end
 end
