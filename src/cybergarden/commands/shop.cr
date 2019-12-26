3e5011e931773312879e863ad46a5b427c71b6f5
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 21:33:06 +0200

    Made the shop "richer"

--- src/cybergarden/commands/shop.cr
+++ src/cybergarden/commands/shop.cr
@@ -1,17 +1,22 @@
 # pp Cybergarden::Items::CpuTypes[0].price
-DESCRIPTION = Cybergarden::Items::CpuTypes.values.map { |x|
+DESCRIPTION_CPU = Cybergarden::Items::CpuTypes.values.map { |x|
     curr = Cybergarden::Items::CpuTypes.from_value(x.to_i).to_cpu
     curr.description
 }.unshift("**CPU SHOP**").join("\n")
 
+DESCRIPTION_SERVER = Cybergarden::Items::ServerTypes.map { |x|
+    x.description
+}.unshift("**SERVERS SHOP**").join("\n")
 def Cybergarden::Commands.shop(cybergarden : Cybergarden::Client,
                                         message : Discord::Message,
                                         args : Array(String)) : Nil
 
+    # TODO fix when garden is not available
     garden = Cybergarden::Utilities.get_garden(message.author.id, cybergarden)
+    
     money = Cybergarden::Utilities.add_money_accordingly(message.author.id, garden.last_money_gain, garden, cybergarden.rethink.connection)
     
-    embed = Discord::Embed.new(
+    embed_cpu = Discord::Embed.new(
         title: "The great hardware shop!",
         timestamp: Time.utc,
         fields: [
@@ -21,9 +26,22 @@ def Cybergarden::Commands.shop(cybergarden : Cybergarden::Client,
                 value: money.humanize(precision: 2)
             )
         ],
-        description: DESCRIPTION,
+        description: DESCRIPTION_CPU,
         footer: Discord::EmbedFooter.new("To buy a new CPU type\n #{Cybergarden::PREFIX}buy CPU <number> <amount> <servername> (e.g: buy CPU 0 1 ProCyber)")
     )
 
-    cybergarden.client.create_message(message.channel_id, content: "", embed: embed)
+    embed_server = Discord::Embed.new(
+        timestamp: Time.utc,
+        fields: [
+            Discord::EmbedField.new(
+                inline: true,
+                name: "Balance",
+                value: money.humanize(precision: 2)
+            )
+        ],
+        description: DESCRIPTION_SERVER,
+        footer: Discord::EmbedFooter.new("To buy a new server type\n #{Cybergarden::PREFIX}buy SERVER <number> <amount> <servername> (e.g: buy server 0 1 ProCyber)")
+    )
+    cybergarden.client.create_message(message.channel_id, content: "", embed: embed_cpu)
+    cybergarden.client.create_message(message.channel_id, content: "", embed: embed_server)
 end
