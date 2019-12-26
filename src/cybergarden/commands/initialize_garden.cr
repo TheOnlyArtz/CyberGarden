a23ac48a5761ce0270dc86aa43af1ad2c5e0d321
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 17:13:26 +0200

    Using the utilities namespace

--- src/cybergarden/commands/initialize_garden.cr
+++ src/cybergarden/commands/initialize_garden.cr
@@ -1,8 +1,8 @@
 def Cybergarden::Commands.initialize_garden(cybergarden : Cybergarden::Client,
                                             message : Discord::Message,
                                              args : Array(String)) : Nil
-                                             
-    garden = get_garden(message.author.id, cybergarden)
+
+    garden = Cybergarden::Utilities.get_garden(message.author.id, cybergarden)
     if garden != nil
     cybergarden.client.create_message(message.channel_id, "Hey TheOnlyArtz, You already own a garden, type ;garden to see it.")
     return
