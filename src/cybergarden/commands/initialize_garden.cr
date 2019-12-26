693701422812f6631d4e9039712de68b6e7335ae
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 17:30:27 +0200

    indentations

--- src/cybergarden/commands/initialize_garden.cr
+++ src/cybergarden/commands/initialize_garden.cr
@@ -4,15 +4,15 @@ def Cybergarden::Commands.initialize_garden(cybergarden : Cybergarden::Client,
 
     garden = Cybergarden::Utilities.get_garden(message.author.id, cybergarden)
     if garden != nil
-    cybergarden.client.create_message(message.channel_id, "Hey TheOnlyArtz, You already own a garden, type ;garden to see it.")
-    return
+        cybergarden.client.create_message(message.channel_id, "Hey TheOnlyArtz, You already own a garden, type ;garden to see it.")
+        return
     end
     
     payload = {
-    id: message.author.id.to_s,
-    money: 200,
-    last_money_gain: Time.utc.to_unix,
-    servers: [] of Nil
+        id: message.author.id.to_s,
+        money: 200,
+        last_money_gain: Time.utc.to_unix,
+        servers: [] of Nil
     }
 
     r.db("cybergarden")
