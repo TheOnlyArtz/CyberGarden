1305ad1a6bd20b4e28ace728902ed468c085c368
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 21:33:55 +0200

    A more pleasent init experience

--- src/cybergarden/commands/initialize_garden.cr
+++ src/cybergarden/commands/initialize_garden.cr
@@ -2,15 +2,14 @@ def Cybergarden::Commands.initialize_garden(cybergarden : Cybergarden::Client,
                                             message : Discord::Message,
                                              args : Array(String)) : Nil
 
-    garden = Cybergarden::Utilities.get_garden(message.author.id, cybergarden)
-    if garden != nil
+    if Cybergarden::Utilities.get_garden?(message.author.id, cybergarden)
         cybergarden.client.create_message(message.channel_id, "Hey TheOnlyArtz, You already own a garden, type ;garden to see it.")
         return
     end
     
     payload = {
         id: message.author.id.to_s,
-        money: 200,
+        money: 12000,
         last_money_gain: Time.utc.to_unix,
         servers: [] of Nil
     }
@@ -20,7 +19,7 @@ def Cybergarden::Commands.initialize_garden(cybergarden : Cybergarden::Client,
     .insert(payload)
     .run(cybergarden.rethink.connection)
 
-    content = "Congrats #{message.author.username}! I've created a fresh new garden for you!"
+    content = "Congrats #{message.author.username}! I've created a fresh new garden for you!\nwith some money to start off!"
     cybergarden.client.create_message(message.channel_id, content)
 
     nil
