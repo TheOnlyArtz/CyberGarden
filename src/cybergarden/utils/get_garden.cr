6b434092e16f715aac1b90a20dbbf3589927b8ec
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 17:13:11 +0200

    Added a utility which gets and parses a garden from the DB

---
+++ src/cybergarden/utils/get_garden.cr
@@ -0,0 +1,8 @@
+def Cybergarden::Utilities.get_garden(id : Discord::Snowflake, cybergarden : Cybergarden::Client) : Cybergarden::Garden
+  raw = r.db("cybergarden")
+    .table("gardens")
+    .get(id.to_s)
+    .run(cybergarden.rethink.connection)
+
+  Cybergarden::Garden.new raw.as_h
+end
