edbdecb7f068047075ef938f8e87a4db91bf7598
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 21:32:37 +0200

    A utility to add a new server to a user's garden

---
+++ src/cybergarden/utils/add_server.cr
@@ -0,0 +1,18 @@
+def Cybergarden::Utilities.add_server(type : Int32, garden : Cybergarden::Garden, money : Int32, name : String, cybergarden : Cybergarden::Client) : Nil
+    garden_db = r.db("cybergarden")
+    .table("gardens")
+    .get(garden.owner_id)
+
+    if garden.servers.find {|d| d.type == type} == nil
+        garden_db
+        .update { |u|
+            {
+                servers: u.get_field("servers").append({type: type, processors: [] of Nil, name: name}),
+                money: money - Cybergarden::Items::ServerTypes[type].price
+            }
+        }
+        .run(cybergarden.rethink.connection)
+    end
+
+    nil
+end
