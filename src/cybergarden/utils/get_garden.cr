cc7a7d6ff9cc4747a0b89f6703c0a266e515f008
callcraft456@gmail.com <callcraft456@gmail.com>
2019-12-26 21:32:21 +0200

    Added a utilty to check whether a garden exists

--- src/cybergarden/utils/get_garden.cr
+++ src/cybergarden/utils/get_garden.cr
@@ -6,3 +6,12 @@ def Cybergarden::Utilities.get_garden(id : Discord::Snowflake, cybergarden : Cyb
 
   Cybergarden::Garden.new raw.as_h
 end
+
+def Cybergarden::Utilities.get_garden?(id : Discord::Snowflake, cybergarden : Cybergarden::Client) : Bool
+  raw = r.db("cybergarden")
+    .table("gardens")
+    .get(id.to_s)
+    .run(cybergarden.rethink.connection)
+
+  raw != nil
+end
